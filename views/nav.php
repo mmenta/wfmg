d1407ac32975de6441273f67e031722bf6456d1a
Menta <xmmenta@admins-MBP-2.socal.rr.com>
2016-01-18 23:47:23 -0800

    removed photo menu

--- views/nav.php
+++ views/nav.php
@@ -3,7 +3,7 @@
         <li><a class="about" href="#">About</a></li>
         <li><a class="property" href="#">Videos</a></li>
         <li><a class="pricing" href="#">Pricing</a></li>
-        <li><a class="photography" href="#">Photos</a></li>
+        <!--<li><a class="photography" href="#">Photos</a></li>-->
         <li><a class="contact" href="#">Contact</a></li>
     </ul>
 </nav>
